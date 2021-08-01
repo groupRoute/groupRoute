12b413330a490081023e0e641d3d0f8c5f2af410
Samuel Mashil <samuel.mashil@yahoo.co.uk>
2021-08-01 13:06:37 +0100

    Added map

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # groupRoute
 
-finna be the next million dollar business :)
+finna be the next million dollar business :), def
 
 # Plan
 
@@ -10,3 +10,12 @@ Save location functionality
 Recommended locations
 
 no tabs on bottom
+
+
+email to send the thing to when we're done: info@investin.org
+Closes Feb 25th
+
+Winners announced: March 15th
+Awards Ceremony: May 5th
+
+:)
--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-
+import { StyleSheet, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>groupRoute</Text>
+      <Text style={styles.title}>groupRoute Map</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 51.5269,
+          longitude: -0.1263,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* {this.state.markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))} */}
+      </MapView>
+      {/* <MapView style={styles.map} /> */}
       <View
         style={styles.separator}
         lightColor="#eee"
@@ -33,5 +51,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
