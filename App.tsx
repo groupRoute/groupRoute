ef1166b2fe51d939f71e19ce6dba11ec7fde6727
Samuel Mashil <samuel.mashil@yahoo.co.uk>
2021-08-07 23:06:32 +0100

    added Firebase config

--- App.tsx
+++ App.tsx
@@ -6,6 +6,21 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import firebase from "firebase/app";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDwcw9VuuzMKtKHtq7NwYnHTPhTzVXQi_Q",
+  authDomain: "grouproute-investin.firebaseapp.com",
+  databaseURL: "https://grouproute-investin-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "grouproute-investin",
+  storageBucket: "grouproute-investin.appspot.com",
+  messagingSenderId: "667536391661",
+  appId: "1:667536391661:web:0f74dd9bcd883969bbbbe4",
+  measurementId: "G-V31GQC18WL"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
